b1d20d9a3f626859278fca88dbcc2468c76fb398
weiweieuroo <korawit.in.57@ubu.ac.th>
2017-07-20 11:46:54 +0700

    ...

--- app/src/main/java/com/example/localjob/fragment/JobListFragment.kt
+++ app/src/main/java/com/example/localjob/fragment/JobListFragment.kt
@@ -7,55 +7,39 @@ import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.ListView
 import com.example.localjob.R
 import com.example.localjob.adapter.CategoryListAdapter
 import com.example.localjob.toolbar.SearchToolbar
 import com.malinskiy.superrecyclerview.SuperRecyclerView
 import kotlinx.android.synthetic.main.fragment_main.view.*
-import kotlinx.android.synthetic.main.item_category.view.*
 
 /**
  * Created by schecterza on 7/18/2017 AD.
  */
 
-class MainFragment: Fragment() {
-    lateinit var category:View
-    lateinit var image:ImageView
+class JobListFragment: Fragment() {
+
 
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var rootView = inflater!!.inflate(R.layout.fragment_main, container, false)
+        var rootView = inflater!!.inflate(R.layout.fragment_job_list, container, false)
         initInstances(rootView)
         return rootView
     }
 
     fun initInstances(rootView: View) {
-        initSearchToolbar()
-        category = rootView.layout_category
-        image = category.category1
-        image.setOnClickListener {
-            fragmentManager.beginTransaction()
-                    .replace(R.id.content_container,JobListFragment.newInstance())
-                    .addToBackStack(null)
-                    .commit()
-        }
 
 
 
 
     }
 
-    fun initSearchToolbar() {
-        fragmentManager.beginTransaction()
-                .add(R.id.search_toolbar, SearchToolbar.newInstances())
-                .commit()
-    }
+
 
     companion object {
-        fun newInstance(): MainFragment {
-            var fragment = MainFragment()
+        fun newInstance(): JobListFragment {
+            var fragment = JobListFragment()
             var args = Bundle()
             fragment.arguments = args
             return fragment
