e5c7211455224dbc5d7d148bf246344bb1e372d4
phuengton <phuengton.ch.57@ubu.ac.th>
2017-07-20 14:39:54 +0700

    ...

--- app/src/main/java/com/example/localjob/custom_view/ListJob.kt
+++ app/src/main/java/com/example/localjob/custom_view/ListJob.kt
@@ -1,10 +1,15 @@
 package com.example.localjob.custom_view
 
 import android.content.Context
+import android.content.Intent
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
+import android.widget.LinearLayout
 import com.example.localjob.R
+import com.example.localjob.StupidActivity
+import com.example.localjob.fragment.WorkDetailFragment
+import kotlinx.android.synthetic.main.item_list_job.view.*
 
 
 class ListJob @JvmOverloads constructor(
@@ -12,8 +17,26 @@ class ListJob @JvmOverloads constructor(
         attrs: AttributeSet? = null,
         defStyleAttr: Int = 0
 ) : FrameLayout (context,attrs,defStyleAttr){
+
+    var itemJob: LinearLayout
+
     init {
+        initInflate()
+
+        this.itemJob = item_job
+        itemJob.setOnClickListener{ goToJobDetail() }
+
+    }
+
+    fun goToJobDetail() {
+        var intent = Intent(context, StupidActivity::class.java)
+        context.startActivity(intent)
+    }
+
+    fun initInflate() {
         LayoutInflater.from(context).inflate(R.layout.item_list_job, this, true)
     }
+
+
 
 }
